4a2253a3c4e9ca9bdf6765ddba0fa83dc742b2dd
Patrick Cook <pcook01@calpoly.edu>
2018-02-26 20:28:43 -0800

    Add Users PUT and DELETE

--- router/routes/users.js
+++ router/routes/users.js
@@ -5,13 +5,33 @@ var router = express.Router();
 
 router.baseURL = '/api/users'
 
+const AllowedFields = {
+  putUser: ["username", "first_name", "last_name", "password_hash"]
+}
+
+const RequiredFields = {
+  postUser: ["username", "first_name", "last_name", "password_hash"]
+}
 /* GET /api/users/
  * Get list of users.
  * Requires admin permissions to receive all users or returns just the AU
  */
 router.get('/', function(req, res, next) {
-  req.db.users.findAll().then(users => {
-      res.json(users).status(200).end()
+  req.db.users.findAll({
+    attributes: ['id', 'username', 'first_name', 'last_name']
+  })
+  .then(users => {
+      res.json({
+          status: "success",
+          data: users
+      }).status(200).end()
+  })
+  .catch(error => {
+      res.json({
+        status: "error",
+        error: error,
+        data: []
+      })
   });
 });
 
@@ -27,7 +47,7 @@ router.post('/', function(req, res, next) {
 
   async.waterfall([
   function(cb) {
-    if (vld.hasFields(body, ["username", "first_name", "last_name", "role"], cb)) {
+    if (vld.hasFields(body, RequiredFields.postUser, cb)) {
       req.db.users.findOrCreate({
         where: {
           username: body.username
@@ -37,10 +57,28 @@ router.post('/', function(req, res, next) {
           first_name: body.first_name,
           last_name: body.last_name,
           role: body.role,
+          password_hash: body.password_hash
         }
       })
       .then(user => {
-        res.json(user);
+          res.json({
+              status: "success",
+              data: {
+                id: user[0].id,
+                username: user[0].username,
+                first_name: user[0].first_name,
+                last_name: user[0].last_name,
+                role: user[0].role
+
+              }
+          }).status(200).end()
+      })
+      .catch(error => {
+          res.json({
+            status: "error",
+            error: error,
+            data: []
+          })
       });
     }
   }],
@@ -56,7 +94,17 @@ router.post('/', function(req, res, next) {
  * Returns list of groups a user is part of
  */
 router.get('/:id', function(req, res, next) {
-  res.json("Get user " + req.params.id);
+  req.db.users.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['username', 'first_name', 'last_name']
+  })
+  .then(user => {
+    if (user)
+      res.json(user);
+    res.status(404).json("User not found")
+  });
 });
 
 /* PUT /api/users/:id
@@ -65,14 +113,51 @@ router.get('/:id', function(req, res, next) {
  * Primarily used for refreshing Spotify access tokens
  */
 router.put('/:id', function(req, res, next) {
-  res.json("Update user " + req.params.id);
+  let body = req.body
+  let vld = req.validator
+  console.log(req.body)
+
+  async.waterfall([
+  function(cb) {
+    if (vld.allowOnlyFields(body, AllowedFields.putUser, cb)) {
+      req.db.users.update(body, { where: {id: req.params.id }})
+      .then(user => {
+        res.json({
+          status: "success",
+          data: user
+        })
+      })
+      .catch(error => {
+          res.json({
+            status: "error",
+            error: error,
+            data: []
+          })
+      });
+    }
+  }],
+  function (error) {
+    res.status(200).end();
+  });
 });
 
 /* DELETE /api/users/:id
  * Allows an Admin to delete a user
  */
 router.delete('/:id', function(req, res, next) {
-  res.json("Delete user " + req.params.id);
+  req.db.users.destroy({where: {id: req.params.id}})
+  .then(user => {
+      res.json({
+          status: "success"
+      }).status(200).end()
+  })
+  .catch(error => {
+      res.json({
+        status: "error",
+        error: error,
+        data: []
+      })
+  });
 });
 
 module.exports = router;

1d7d460efd417a3c61497efd37d3918c693bb040
Patrick Cook <pcookcollege@gmail.com>
2019-03-03 18:13:54 -0800

    Set port number

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.use(function(req, res, next) {
 });
 
 db.sequelize.sync().then(() => {
-  app.listen(3000, () => {
-    console.log('Express listening on port:', 3000);
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('Express listening on port:', process.env.PORT || 3000);
   });
 });
